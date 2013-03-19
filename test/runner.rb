044585582cc709cbc7c3e38bd7af76d5338e58be
7rans <transfire@gmail.com>
2013-03-19 12:31:46 -0400

    Raise error if FileUtils is already loaded. [test]

--- test/runner.rb
+++ test/runner.rb
@@ -1,7 +1,5 @@
 if defined?(::FileUtils)
-  warn "Fail! FileUtils is already loaded!"
-else
-  warn "Okay! FileUtils is not already loaded."
+  raise "FileUtils is already loaded!"
 end
 
 root = File.expand_path('..', File.dirname(__FILE__))
@@ -9,7 +7,7 @@ root = File.expand_path('..', File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(root, 'test'))
 $LOAD_PATH.unshift(File.join(root, 'lib'))
 
-require 'minitest/autorun'
+#require 'minitest/autorun'
 
 test_files = Dir[File.join(root, 'test', 'fileutils', 'test_*.rb')] 
 test_files.each do |test_file|
