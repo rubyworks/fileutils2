3dccf4998479a3de8108e56c213f44a042e0a2fb
7rans <transfire@gmail.com>
2013-03-19 14:25:01 -0400

    Use the latest minitest gem. [admin]

--- test/runner.rb
+++ test/runner.rb
@@ -7,7 +7,7 @@ root = File.expand_path('..', File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(root, 'test'))
 $LOAD_PATH.unshift(File.join(root, 'lib'))
 
-#require 'minitest/autorun'
+require 'minitest/autorun'
 
 test_files = Dir[File.join(root, 'test', 'fileutils2', 'test_*.rb')] 
 test_files.each do |test_file|
