6215c7feb7648055ba1f751e81f48c88b08ec8dd
7rans <transfire@gmail.com>
2013-03-19 12:01:59 -0400

    Try loading minitest/autorun. [test]

--- test/runner.rb
+++ test/runner.rb
@@ -4,14 +4,13 @@ begin
 rescue
 end
 
-require 'minitest/autorun'
-
 root = File.expand_path('..', File.dirname(__FILE__))
 
 $LOAD_PATH.unshift(File.join(root, 'lib'))
 
-test_files = Dir[File.join(root, 'test', 'fileutils', 'test_*.rb')] 
+require 'minitest/autorun'
 
+test_files = Dir[File.join(root, 'test', 'fileutils', 'test_*.rb')] 
 test_files.each do |test_file|
   require test_file
 end
